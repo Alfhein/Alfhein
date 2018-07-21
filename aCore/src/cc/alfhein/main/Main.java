e2a1b7a741a346855ec84b4402aecab545c79139
Alfhein <admin@alfhein.cc>
2018-07-21 15:29:12 -0700

    Added onEnable and onDisable

--- aCore/src/cc/alfhein/main/Main.java
+++ aCore/src/cc/alfhein/main/Main.java
@@ -4,6 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
-	
+	public void onEnable() {
+		getServer().getConsoleSender().sendMessage("aCore Started");
+	}
+	public void onDisable() {
+		getServer().getConsoleSender().sendMessage("aCore Disabled");
+	}
 
 }
