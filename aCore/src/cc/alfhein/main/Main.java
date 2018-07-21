bd4c966c8dc47a7a300b32194c2e23404d3d92ce
Distracter <contactvirizin@gmail.com>
2018-07-21 18:45:46 -0400

    gtrf
    gtfvc
    
    last update

--- aCore/src/cc/alfhein/main/Main.java
+++ aCore/src/cc/alfhein/main/Main.java
@@ -1,14 +1,42 @@
 package cc.alfhein.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	public static Main instance;
+	
+	@Override
 	public void onEnable() {
-		getServer().getConsoleSender().sendMessage("aCore Started");
+		PluginManager pm = Bukkit.getPluginManager();
+		
+		getLogger().info("Initalizing aCore v0.1");
+		
+		// Load Listeners
+		
+		// Load Commands
+		
+		getLogger().info("Enabled!");
+		
+		saveDefaultConfig();
 	}
+	
+	@Override
 	public void onDisable() {
-		getServer().getConsoleSender().sendMessage("aCore Disabled");
+		getLogger().info("Disabled!");
+		
+		saveConfig();
+	}
+	
+	public static Main getInstance() {
+		return instance;
+	}
+	
+	public static String color(String message) {
+		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 
 }
